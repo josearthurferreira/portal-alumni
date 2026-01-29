d3d4f282ccc702d2abd326cbe2cf9a47cb943756
fnmiranda <r20franciscomiranda@gmail.com>
2026-01-29 11:51:53 -0300

    fix responsive header, and add logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,6 +3,8 @@ import { Plus, LogOut } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 
+import localImage from '/logo.webp';
+
 const Header = ({
   onAddClick,
   isLoggedIn,
@@ -20,12 +22,16 @@ const Header = ({
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
-        <h1 className={styles.logo} onClick={() => navigate('/')}>
-          IME Alumni
+        <h1 className={styles.titleLogo} onClick={() => navigate('/')}>
+          <div className={styles.logoWrapper}>
+            <img src={localImage} alt="Logo alumni" width={80} height={100} />
+          </div>
+          <div className={styles.titleAlum}>Alumni</div>
+          
         </h1>
 
         <div className={styles.actions}>
-          {isLoggedIn ? (
+          {!isLoggedIn ? (
             <>
               <button className={styles.addBtn} onClick={onAddClick}>
                 <Plus size={18} /> {addLabel}
@@ -43,6 +49,10 @@ const Header = ({
             </button>
           )}
         </div>
+      </div>
+      <div className={styles.subtitle}>
+        <div></div>
+        <strong>ENCONTRE EX-ALUNOS DO IME</strong>
       </div>
     </header>
   );
